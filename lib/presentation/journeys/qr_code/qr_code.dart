ff88be9697ceedea6fd542ad9089e3ebe0e0d8da
Imdadul <imdadulh@mridayaitservices.com>
2021-08-26 21:00:55 +0530

    Static App UI

--- lib/presentation/journeys/qr_code/qr_code.dart
+++ lib/presentation/journeys/qr_code/qr_code.dart
@@ -1,18 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:qcharge_flutter/presentation/journeys/drawer/navigation_drawer.dart';
 import 'package:qcharge_flutter/presentation/journeys/qr_code/finish.dart';
-import 'package:qcharge_flutter/presentation/journeys/qr_code/start.dart';
 import 'package:qcharge_flutter/presentation/widgets/app_bar_home.dart';
 
-class QrCode extends StatelessWidget{
+import 'next.dart';
+import 'start.dart';
+import 'stop.dart';
+
+class QrCode extends StatefulWidget{
+
+  @override
+  _QrCodeState createState() => _QrCodeState();
+}
+
+class _QrCodeState extends State<QrCode> {
+  late String screenTitle = 'next';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBarHome(context),
       drawer: NavigationDrawer(),
-      body: Finish(),
-
+      body: loadNextScreen(),
     );
   }
 
+  Widget loadNextScreen(){
+    print('----Screen Title : $screenTitle');
+    switch (screenTitle) {
+      case 'next':
+        return Next(onTap:(){
+          setState(() {
+            screenTitle = 'start';
+          });
+        },);
+      case 'start':
+        return Start(onTap:(){
+          setState(() {
+            screenTitle = 'stop';
+          });
+        },);
+      case 'stop':
+        return Stop(onTap:(){
+          setState(() {
+            screenTitle = 'finish';
+          });
+        },);
+      case 'finish':
+        return Finish(onTap:(){
+          setState(() {
+            screenTitle = 'next';
+          });
+        },);
+      default:
+        return Next(onTap:(){
+          setState(() {
+            setState(() {
+              screenTitle = 'next';
+            });
+          });
+        },);
+    }
+  }
 }
--- lib/presentation/journeys/qr_code/stop.dart
+++ lib/presentation/journeys/qr_code/stop.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_animator/flutter_animator.dart';
 import 'package:qcharge_flutter/common/constants/size_constants.dart';
 import 'package:qcharge_flutter/common/extensions/size_extensions.dart';
 import 'package:qcharge_flutter/presentation/themes/theme_color.dart';
@@ -7,77 +8,86 @@ import 'package:qcharge_flutter/presentation/widgets/button.dart';
 import 'package:qcharge_flutter/presentation/widgets/txt_ic_row.dart';
 
 class Stop extends StatelessWidget {
+  final Function onTap;
+
+  const Stop({Key? key, required this.onTap}) : super(key: key);
+
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Container(
-          width: Sizes.dimen_280.w,
-          padding: const EdgeInsets.all(24),
-          margin: const EdgeInsets.only(bottom: 12),
-          decoration: BoxDecoration(
-            color: AppColor.grey,
-            borderRadius: BorderRadius.all(Radius.circular(8.0)),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ImgTxtRow(
-                txt: 'Charging time 00:00:40',
-                txtColor: AppColor.app_txt_white,
-                txtSize: 12,
-                fontWeight: FontWeight.normal,
-                icon: 'assets/icons/pngs/scan_qr_for_filter_5.png',
-                icColor: AppColor.app_txt_white,
-              ),
-              ImgTxtRow(
-                txt: 'Unit: 1.56 kWh',
-                txtColor: AppColor.app_txt_white,
-                txtSize: 12,
-                fontWeight: FontWeight.normal,
-                icon: 'assets/icons/pngs/scan_qr_for_filter_4.png',
-                icColor: AppColor.app_txt_white,
-              ),
+    return SlideInRight(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: Sizes.dimen_280.w,
+            padding: const EdgeInsets.all(24),
+            margin: const EdgeInsets.only(bottom: 12),
+            decoration: BoxDecoration(
+              color: AppColor.grey,
+              borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ImgTxtRow(
+                  txt: 'Charging time 00:00:40',
+                  txtColor: AppColor.app_txt_white,
+                  txtSize: 12,
+                  fontWeight: FontWeight.normal,
+                  icon: 'assets/icons/pngs/scan_qr_for_filter_5.png',
+                  icColor: AppColor.app_txt_white,
+                ),
+                ImgTxtRow(
+                  txt: 'Unit: 1.56 kWh',
+                  txtColor: AppColor.app_txt_white,
+                  txtSize: 12,
+                  fontWeight: FontWeight.normal,
+                  icon: 'assets/icons/pngs/scan_qr_for_filter_4.png',
+                  icColor: AppColor.app_txt_white,
+                ),
 
-              ImgTxtRow(
-                txt: 'AC Type: 2 22km',
-                txtColor: AppColor.app_txt_white,
-                txtSize: 12,
-                fontWeight: FontWeight.normal,
-                icon: 'assets/icons/pngs/scan_qr_for_filter_10_charge_ac.png',
-                icColor: AppColor.app_txt_white,
+                ImgTxtRow(
+                  txt: 'AC Type: 2 22km',
+                  txtColor: AppColor.app_txt_white,
+                  txtSize: 12,
+                  fontWeight: FontWeight.normal,
+                  icon: 'assets/icons/pngs/scan_qr_for_filter_10_charge_ac.png',
+                  icColor: AppColor.app_txt_white,
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/scan_qr_for_filter_9_next.png',
+                height: Sizes.dimen_130.h,
+                width: Sizes.dimen_110.w,
+              ),
+              Image.asset(
+                'assets/icons/pngs/scan_qr_for_filter_6.png',
+                height: Sizes.dimen_150.h,
+                width: Sizes.dimen_250.w,
               ),
             ],
           ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/scan_qr_for_filter_9_next.png',
-              height: Sizes.dimen_130.h,
-              width: Sizes.dimen_110.w,
+          Padding(
+            padding: const EdgeInsets.only(left: 36, right: 36, bottom: 36),
+            child: Button(
+              text: 'STOP',
+              bgColor: Colors.white,
+              onPressed: () {
+                onTap();
+              },
             ),
-            Image.asset(
-              'assets/icons/pngs/scan_qr_for_filter_6.png',
-              height: Sizes.dimen_150.h,
-              width: Sizes.dimen_250.w,
-            ),
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 36, right: 36, bottom: 36),
-          child: Button(
-            text: 'START',
-            bgColor: Colors.white,
-            onPressed: () {},
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
